3a9e96639b36de6e22530d6c21d84a2a32fa1105
Vítor Norberto <vithor.norberto@gmail.com>
2020-04-14 20:59:10 -0300

    Commit inicial

---
+++ src/main/java/com/audora/comprasonline/api/ComprasOnlineApiApplication.java
@@ -0,0 +1,13 @@
+package com.audora.comprasonline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComprasOnlineApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComprasOnlineApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/audora/comprasonline/api/ComprasOnlineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audora.comprasonline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComprasOnlineApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
