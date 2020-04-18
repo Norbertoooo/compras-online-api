8681c3998110cef13b11908da2f2568e091327a9
Vítor Norberto <vithor.norberto@gmail.com>
2020-04-18 15:54:40 -0300

    Adiciona produtos e usuario base

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,12 @@
 insert into categoria_produto(nome) values ('higiene'), ('alimento'), ('limpeza'), ('bebidas');
+
+insert into usuario(email, senha) values ('audora@gmail.com', '$2y$12$Pq4MjfdvHNF/zN5K8fC/quCcICrS7AQFsOnkOAFYvGx7MKR./LlFy');
+
+insert into produto(nome,preco,categoria_id) values ('shampo',10.50,1);
+insert into produto(nome,preco,categoria_id) values ('frango', 10.50,2);
+insert into produto(nome,preco,categoria_id) values ('sabao', 4.00,3);
+insert into produto(nome,preco,categoria_id) values ('coca', 5.00,4);
+
+
+
+
