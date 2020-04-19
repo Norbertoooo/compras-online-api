35f380a50bef9743f429144fb838a7b3bdc04589
Vítor Norberto <vithor.norberto@gmail.com>
2020-04-19 20:54:23 -0300

    Altera versão do swagger

--- src/main/java/com/audora/comprasonline/api/config/SwaggerConfig.java
+++ src/main/java/com/audora/comprasonline/api/config/SwaggerConfig.java
@@ -9,6 +9,8 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.ApiKeyVehicle;
+import springfox.documentation.swagger.web.SecurityConfiguration;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -28,6 +30,11 @@ public class SwaggerConfig {
         return new ApiInfoBuilder().title("Sistemas de compras online API")
                 .description("Documentação da API de acesso aos endpoints.").version("1.0")
                 .build();
+    }
+
+    @Bean
+    SecurityConfiguration security() {
+        return new SecurityConfiguration(null, null, null, null, "Bearer access_token", ApiKeyVehicle.HEADER, "Authorization", ",");
     }
 
 }
