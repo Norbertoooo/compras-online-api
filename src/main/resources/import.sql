ba1596536d20bfacd339f140835331ef5104e5b6
Vítor Norberto <vithor.norberto@gmail.com>
2020-04-21 17:40:24 -0300

    Adiciona um usuario cliente

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,8 @@
 insert into categoria_produto(nome) values ('higiene'), ('alimento'), ('limpeza'), ('bebidas');
 
 insert into usuario(email, senha, admin) values ('audora@gmail.com', '$2y$12$Pq4MjfdvHNF/zN5K8fC/quCcICrS7AQFsOnkOAFYvGx7MKR./LlFy', TRUE);
+insert into usuario(email, senha, admin) values ('cliente@gmail.com', '$2y$12$Pq4MjfdvHNF/zN5K8fC/quCcICrS7AQFsOnkOAFYvGx7MKR./LlFy', FALSE);
+
 
 insert into produto(nome,preco,categoria_id) values ('shampo',10.50,1);
 insert into produto(nome,preco,categoria_id) values ('frango', 10.50,2);
